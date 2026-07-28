6ed5efbbb908e468763ca55cfd936c2fcc6cace6
Mubashshir-Dev <mubashshir.tech@gmail.com>
2026-07-29 00:27:56 +0530

    searchElementIn_a_RotatedSortedArray

---
+++ bs/searchElementIn_a_RotatedSortedArray/opt.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+int search(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size()-1;
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        if(nums[mid]==target)return mid;
+        if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && nums[mid]>=target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+            }
+        else{
+            if(nums[high]>=target && nums[mid]<=target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+    }
+    return -1;
+}
+
+
+int main(){
+    int size;
+    cin>>size;
+    vector <int> arr(size);
+    for(int i = 0 ; i < size ; i++){
+        cin>>arr[i];
+    }
+    int key;
+    cin>>key;
+    cout<<endl;
+    cout<<ls(arr,key)<<endl;
+    return 0;
+}
